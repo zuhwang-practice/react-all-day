e718384d20e70fe8321fbefc1136e9de7861c8dc
zu-hwang <tropical.kitsch@gmail.com>
2020-07-28 19:19:17 +0900

    fin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,16 +4,16 @@ import SNSLogin from '../SNSLogin';
 import './Login.css';
 
 class Login extends Component {
-	render() {
-		return (
-			<div className='Login' style={{ backgroundColor: this.props.LoginColor }}>
-				<div className='login-wrap'>Login</div>
-				<button onClick={this.props.handleLogin}>CHANNNNNGE</button>
-				{/* 자식컴포넌트의이벤트로 부모의 state를 변경하려면 props로 이벤트함수를 전달받아야 한다. */}
-				<SNSLogin />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='Login' style={{ backgroundColor: this.props.LoginColor }}>
+        <div className='login-wrap'>Login</div>
+        <button onClick={this.props.handleLogin}>CHANNNNNGE</button>
+        {/* 자식컴포넌트의이벤트로 부모의 state를 변경하려면 props로 이벤트함수를 전달받아야 한다. */}
+        <SNSLogin />
+      </div>
+    );
+  }
 }
 
 export default Login;
---
+++ src/components/SNSLogin.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const SNSLogin = () => {
+  return <div></div>;
+};
+
+export default SNSLogin;
